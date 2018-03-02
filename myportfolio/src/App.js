5e820916a92a4eb1844486f28ecd02da77fc52fe
Taylor Hampton <taylor@curateurs.com>
2018-03-01 20:24:33 -0500

    navbar and layout components

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="TWH Portfolio" style={{color: 'white'}}>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="TWH Portfolio">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
+
     );
   }
 }
