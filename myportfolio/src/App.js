9d01f4c0972a89e3cfe581b558933303ab9de4c0
Taylor Hampton <taylor@curateurs.com>
2018-03-03 12:46:55 -0500

    routes and nav complete

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,31 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="TWH Portfolio" style={{color: 'white'}}>
+      <div style={{
+        height: '300px',
+        position: 'relative'
+      }}>
+        <Layout
+          style={{
+          background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'
+        }}>
+          <Header
+            transparent
+            title="TWH Portfolio"
+            style={{
+            color: 'white'
+          }}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutMe">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
-        </Header>
-        <Drawer title="TWH Portfolio">
+          </Header>
+          <Drawer title="TWH Portfolio">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutMe">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
             </Navigation>
-        </Drawer>
-        <Content />
-    </Layout>
-</div>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main/>
+          </Content>
+        </Layout>
+      </div>
+    
 
     );
   }
---
+++ myportfolio/src/components/main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import LandingPage from './landingpage';
+import Projects from './projects';
+import Resume from './resume';
+import Contact from './contact';
+import AboutMe from './aboutme';
+
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contact" component={Contact} />
+
+
+
+    </Switch>
+
+)
+
+export default Main;
